50df34c0ba73ce6cf6f2463fae34938ed8c86697
Natenael Kelkay <natikelkay@gmail.com>
2018-02-16 06:56:28 -0500

    changes to turtle graphics drawing

--- ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/TurtleDrawings.java
+++ ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/TurtleDrawings.java
@@ -6,6 +6,22 @@ import TurtleGraphics.StandardPen;
 
 public class TurtleDrawings {
 	public static void main(String[] args) {
-
+		SketchPadWindow win = new SketchPadWindow(400, 400);
+        Pen p = new StandardPen(win);
+        
+        int move = 1;
+        for (int i = 0; i <360; i++) {
+        	p.down();
+        	p.turn(move);
+        	p.move(100);
+        	p.turn(30);
+        	p.move(60);
+        	p.turn(-30);
+        	p.move(30);
+        	p.up();
+        	p.home();
+        	move+= 1;
+        } 
+        
 	}
 }
