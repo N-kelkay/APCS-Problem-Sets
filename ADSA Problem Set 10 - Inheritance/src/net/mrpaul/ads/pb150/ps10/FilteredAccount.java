cbeee10ac18382cd733e3e0efb0288f9265cbba1
Natenael Kelkay <natikelkay@gmail.com>
2018-02-16 08:49:42 -0500

    modified constructor, in FilteredAccount

--- ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/FilteredAccount.java
+++ ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/FilteredAccount.java
@@ -1,10 +1,12 @@
 package net.mrpaul.ads.pb150.ps10;
 
+import net.mrpaul.ps10.distribution.Account;
 import net.mrpaul.ps10.distribution.Account.Client;
 
-public class FilteredAccount {
-	
-	public FilteredAccount (Client c){
+public class FilteredAccount extends Account{
+
+	public FilteredAccount(Client c) {
+		super(c);
 		
 	}
 }
