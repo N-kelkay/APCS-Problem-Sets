65f98b006b599f64161a646e6964e6b2b3911bd0
Natenael Kelkay <natikelkay@gmail.com>
2018-02-16 08:39:40 -0500

    added class: percentFiltered, and tested the test class

---
+++ ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/FilteredAccount.java
@@ -0,0 +1,10 @@
+package net.mrpaul.ads.pb150.ps10;
+
+import net.mrpaul.ps10.distribution.Account.Client;
+
+public class FilteredAccount {
+	
+	public FilteredAccount (Client c){
+		
+	}
+}
--- ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/harnesses/FilteredAccountHarness.java
+++ ADSA Problem Set 10 - Inheritance/src/net/mrpaul/ads/pb150/ps10/harnesses/FilteredAccountHarness.java
@@ -8,7 +8,6 @@ import net.mrpaul.ps10.distribution.Account.Transaction;
 //Notice the errors?  That's because you haven't yet written FilteredAccount.
 public class FilteredAccountHarness {
 	public static void main(String[] args){
-		
 		FilteredAccount f = new FilteredAccount(new Client());
 		int[] transactionList = {0,0,0,10,30,2000,-50,0,-200,3000000};
 		
